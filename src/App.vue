247c0f0929c95daa2cc7c0b8b2d650b805e17a83
Chabiiiiii <302829947@qq.com>
2017-07-08 08:04:57 +0800

    2017.07.08

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
-    <router-view></router-view>
-    <a href="/">主页</a>
-    <a href="#/regist">deng</a>
-    <a href="#/cart">cart</a>
-    <a href="#/kind">kind</a>
-
+    <router-view class="main"></router-view>
+    <div class="nav">
+      <a href="/"><i class="iconfont">&#xe605;</i>达令</a>
+      <a href="#/kind"><i class="iconfont">&#xe72b;</i>分类</a>
+      <a href="#/cart"><i class="iconfont">&#xe63f;</i>购物车</a>
+      <a href="#/regist"><i class="iconfont">&#xe61b;</i>注册</a>
+    </div>
+    
   </div>
 </template>
 
@@ -16,12 +18,50 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/*公共样式*/
+body,h1,h2,h3,h4,h5,h6,p,ul,ol,li,dl,dt,dd,input,a,img,header,nav,main,section,footer,article,aside,figure,figcaption{padding:0; margin:0;}
+header,nav,main,section,footer,article,aside,figure,figcaption{display:block;}
+ul,ol{list-style:none;}
+input{outline:none;}
+img{border:0;display:block;}
+
+body{font:12px/1.5 "微软雅黑"; color:#3e3e3e; }
+a{text-decoration:none; color:3e3e3e; cursor:pointer;}
+a:hover{text-decoration:none; color:#ca2722;}
+
+.all {
+  display: flex;
+  width: 100%;
+  
+  flex-direction: column;}
+
+html,body{height:100%;}
+
+/*app*/
+#app{
+  display: flex;
+  flex-direction: column;
+  height: 100%
+}
+#app .main{
+    flex:1;
+}
+.nav{
+    height:1rem;
+    display: flex;
+    justify-content: space-around;
+    background-color: #fff;
+    text-align: center;
+    position:fixed;
+    width:100%;
+    bottom:0;
+    font-size: .2rem;
+}
+.nav a{
+  flex:1;
+
+}
+.nav a i{
+  display: block;
 }
 </style>
